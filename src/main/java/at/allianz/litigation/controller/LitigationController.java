b2830ece5ef615b3af0036f3b2eb11ad04a7a293
Lukasz <lukasz.matuszewski@gmail.com>
2024-05-15 21:07:58 +0200

    openapi

--- src/main/java/at/allianz/litigation/controller/LitigationController.java
+++ src/main/java/at/allianz/litigation/controller/LitigationController.java
@@ -3,11 +3,13 @@ package at.allianz.litigation.controller;
 import at.allianz.litigation.domain.Litigation;
 import at.allianz.litigation.repository.LitigationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/litigations")
 public class LitigationController {
 
     @Autowired
@@ -18,43 +20,39 @@ public class LitigationController {
         return "Hello!";
     }
 
-    @GetMapping("/litigations")
+    @GetMapping("/")
     List<Litigation> all() {
         return litigationRepository.findAll();
     }
 
-    @GetMapping("/claim/{claimNumber}/litigations")
+    @GetMapping("/by-claim/{claimNumber}")
     List<Litigation> findByClaimNumber(@PathVariable String claimNumber) {
         return litigationRepository.findAllByClaimNumber(claimNumber);
     }
 
-    @GetMapping("/litigations/{id}")
+    @GetMapping("/{id}")
     Litigation get(@PathVariable String id) {
         return litigationRepository.findById(id).get();
     }
 
-    /*
-    @GetMapping(value = {"/customers/{firstName}", "/customers/"})
-    public ResponseEntity findByFirstName(@PathVariable(required = false) String firstName) {
-        // TODO: proper validation
-        if (firstName == null || firstName.isBlank() || firstName.equals("null")) {
-            return ResponseEntity.badRequest().body(null);
-        }
-        Litigation customer = litigationRepository.findByFirstName(firstName);
-        if (customer == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(customer);
-    }
-     */
-
-    @PostMapping("/litigations")
+    @PostMapping("/")
     Litigation crate(@RequestBody Litigation litigation) {
-        System.out.println("litigation = " + litigation);
         return litigationRepository.save(litigation);
     }
 
-    @DeleteMapping("/litigations/{id}")
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Litigation update(@PathVariable("id") final String id, @RequestBody final Litigation newLitigation) {
+        Litigation litigation = litigationRepository.findById(id).get();
+        litigation.setCaseNumber(newLitigation.getCaseNumber());
+        litigation.setClaimNumber(newLitigation.getClaimNumber());
+        litigation.setType(newLitigation.getType());
+        litigation.setReason(newLitigation.getReason());
+        litigation.setNumberOfJointPlaintiffs(newLitigation.getNumberOfJointPlaintiffs());
+        return litigationRepository.save(litigation);
+    }
+
+    @DeleteMapping("/{id}")
     void delete(@PathVariable String id) {
         litigationRepository.deleteById(id);
     }
