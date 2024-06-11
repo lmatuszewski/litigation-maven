745c5031a43d187d3510cc5b9ac7821c8aec1854
Lukasz <lukasz.matuszewski@gmail.com>
2024-06-11 16:47:17 +0200

    back to jpa

--- src/main/java/at/allianz/litigation/repository/LitigationRepository.java
+++ src/main/java/at/allianz/litigation/repository/LitigationRepository.java
@@ -1,13 +1,14 @@
 package at.allianz.litigation.repository;
 
 import at.allianz.litigation.domain.Litigation;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface LitigationRepository extends MongoRepository<Litigation, String> {
+public interface LitigationRepository extends JpaRepository<Litigation, String> {
 
     List<Litigation> findAllByClaimNumber(String claimNumber);
 }
